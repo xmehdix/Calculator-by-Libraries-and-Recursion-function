4bd8065524283cb0d6dcd38b057ef5d0300b10c1
Mehdi Mohammadi <86534238+xmehdix@users.noreply.github.com>
2022-06-28 21:09:08 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,46 @@
+from art import logo
+from replit import clear
+#Calculator
+
+#Add
+def add(n1,n2):
+  return n1+n2
+
+#Subtract
+def subtract(n1,n2):
+  return n1 - n2
+
+#Multiply
+def multiply(n1,n2):
+  return n1 * n2 
+#Divide
+def divide(n1,n2):
+  return n1/n2
+
+operations= {
+"+":add,
+"-":subtract,
+"*":multiply,
+"/": divide
+}
+def calculator():
+  print(logo)
+  num1=float(input("What's the first number?: "))
+  for symbol in operations:
+    print(symbol)
+  
+  should_continue=True
+  while should_continue==True:
+    operation_symbol= input("Pick an operation: ")
+    num2=float(input("What's the next number?: "))
+    calculation_function= operations[operation_symbol]
+    answer= calculation_function(num1,num2)
+    print(f"{num1} {operation_symbol} {num2} = {answer}")
+    if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to exit.: ")=="y":
+      num1=answer
+    else:
+      should_continue=False
+      clear()
+      calculator()
+      
+calculator()
